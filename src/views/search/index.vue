ca6b2150d940dca365d6472e4afa0f76d009dd80
zhanghao <zhanghao@outlook.com>
2020-01-15 19:43:29 +0800

    文章搜索-处理(历史记录/联想建议/搜索结果)显示状态

--- src/views/search/index.vue
+++ src/views/search/index.vue
@@ -1,24 +1,37 @@
 <template>
   <div class="search-container">
-      <!-- 在van-search 外层增加form标签且action不为空 即可在ios输入法中显示搜索按钮-->
+    <!-- 搜索栏 -->
+    <!-- 在van-search 外层增加form标签且action不为空 即可在ios输入法中显示搜索按钮-->
     <form action="/" slot="left">
-      <van-search v-model="value" @search="onSearch" @cancel="onCancel"
-      placeholder="请输入搜索关键词" show-action background="#3296fa" ></van-search>
+      <van-search v-model="value" @search="onSearch"
+        @cancel="onCancel"
+        placeholder="请输入搜索关键词"
+        show-action
+        background="#3296fa"
+      ></van-search>
     </form>
-    <van-list>
-        <van-cell title="历史记录">
-            <van-icon slot="right-icon" name="delete" color="#969799"/>
-            <span>全部删除</span>
-            <span>&nbsp;&nbsp;完成</span>
-        </van-cell>
-        <van-cell v-for="item in list" :key="item" :title="item">{{历史记录}}</van-cell>
-    </van-list>
-    <van-list>
-        <van-cell v-for="item in 3" :key="item" :title="item" icon="search"></van-cell>
-    </van-list>
+    <!-- /搜索栏 -->
+
     <!-- 搜索结果 -->
-    <search-result/>
+    <search-result v-if="isSearchResultShow" />
     <!-- /搜索结果 -->
+
+    <!-- 联想建议 -->
+    <van-cell-group v-else-if="searchContent">
+      <van-cell v-for="item in 3" :key="item" :title="item" icon="search"></van-cell>
+    </van-cell-group>
+    <!-- /联想建议 -->
+
+    <!-- 历史记录 -->
+    <van-cell-group v-else>
+      <van-cell title="历史记录">
+        <span>全部删除</span>
+        <span>&nbsp;&nbsp;完成</span>
+        <van-icon slot="right-icon" name="delete" color="#969799" />
+      </van-cell>
+      <van-cell v-for="item in list" :key="item" :title="item">历史记录</van-cell>
+    </van-cell-group>
+    <!-- /历史记录 -->
   </div>
 </template>
 <script>
@@ -32,7 +45,9 @@ export default {
   data () {
     return {
       value: '',
-      list: []
+      list: [],
+      isSearchResultShow: 'false', // 搜索结果
+      searchContent: 'false' // 历史记录
     }
   },
   methods: {
@@ -43,5 +58,4 @@ export default {
 </script>
 
 <style lang="less" scoped>
-
 </style>
